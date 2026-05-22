cb8d497f30290f9cc8c14a7c830863f9eee0d644
BracketProto <creepertruck812@gmail.com>
2026-05-22 09:46:06 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -4,20 +4,21 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 function CloneRepoOnStart() {
-  try {
-    const ClonePath = path.join(__dirname, 'app');
+    try {
+        const ClonePath = path.join(__dirname, 'app');
 
-    if (fs.existsSync(ClonePath)) {
-      console.log('Repository already exists at:', ClonePath);
-      return;
+        if (fs.existsSync(ClonePath)) {
+            execSync(`git pull`, { cwd: ClonePath, stdio: 'inherit' });
+            return;
+        }
+
+        console.log('Cloning repository...');
+        execSync(`git clone ${FETCHURL} ${ClonePath}`, { stdio: 'inherit' });
+        console.log('Repository cloned successfully to:', ClonePath);
+    } catch (error) {
+        console.error('Error cloning repository:', error.message);
     }
-    
-    console.log('Cloning repository...');
-    execSync(`git clone ${FETCHURL} ${ClonePath}`, { stdio: 'inherit' });
-    console.log('Repository cloned successfully to:', ClonePath);
-  } catch (error) {
-    console.error('Error cloning repository:', error.message);
-  }
 }
 
 CloneRepoOnStart();
+setInterval(CloneRepoOnStart, 1 * 60 * 1000);
